b065d75e6c1bff3a97b060085a9ff34ac18975fb
Monika <3891monikasingh@gmail.com>
2025-10-19 17:24:58 +0530

    Update while.cpp

--- while.cpp
+++ while.cpp
@@ -20,6 +20,40 @@ int main(){
     }while(i<1);
     return 0;
 }
-//---------------------------------------------------------------------
+
+//-------------------------Examples--------------------------------------------
+int main() {
+    char choice;
+
+    do {
+        float width, height, area;
+        cout << "Enter width and height separated by space: ";
+        cin >> width >> height;
+
+        area = width * height;
+        cout << "Area is: " << area << endl;
+
+        cout << "Calculate another (Y/N): ";
+        cin >> choice;
+
+    } while (choice == 'Y' || choice == 'y');
+
+    cout << "Thanks!" << endl;
+
+    return 0;
+}
+----------input validation-------------
+    int main() {
+    int number;
+
+    do {
+        cout << "Enter an integer between 1 and 5: ";
+        cin >> number;
+    } while (number < 1 || number > 5);  
+
+    cout << "Thanks!" << endl;
+    return 0;
+}
+
 
 
