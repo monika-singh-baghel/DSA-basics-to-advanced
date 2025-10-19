c8403341ec674c6407df7a8afbf6bc6cd9be643d
Monika <3891monikasingh@gmail.com>
2025-10-19 14:33:08 +0530

    Implement do-while loop example
    Add a do-while loop example in while.cpp

---
+++ while.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+//--------------------while loop--------------------------
+/*int main(){
+    int i = 1;
+while(i<5)
+{
+    cout<<"Moni"<<endl;
+    i+=1;
+}
+return 0;
+}*/
+
+//---------------------do while loop------------------------
+int main(){
+    int i = 2;
+    do{
+        cout<<"Moni"<<endl;//do while exectues one time even if the condition is false
+        i+=1;
+    }while(i<1);
+    return 0;
+}
+//---------------------------------------------------------------------
+
+
