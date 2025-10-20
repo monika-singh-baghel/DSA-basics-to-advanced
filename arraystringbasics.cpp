6d306cb5e55e02b48d88ed082ecbaad5fc9e4f52
Monika <3891monikasingh@gmail.com>
2025-10-20 20:01:50 +0530

    Add files via upload

---
+++ arraystringbasics.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*int main(){
+     int a,b,c,d;
+     cin>>a>>b>>c>>d;
+     cout<<a<<b<<c<<d;
+     //arrays are used when we store multiple data that are of same datatypes that overcomes the above problem of renaming and storing multiple variables
+}*/
+//--------------------------------------------------
+/*int main(){
+     int arr[5];
+     cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+     cout<<arr[0]<<arr[1]<<arr[2]<<arr[3]<< arr[4];
+     arr[3] = 15;
+     cout<<arr[3];
+     //arrays are used when we store multiple data that are of same datatypes that overcomes the above problem of renaming and storing multiple variables
+    //double long int etc datatypes can be used in arrays 
+    //consecutive memory allocation and static in nature 
+    }*/
+//--------------------------------2D Array=----------------------------------
+     /*int main(){
+     int arr[3][5];
+     arr[0][0] = 23;
+     cout<<arr[0][0];// prints the assigned value 
+     cout<<arr[1][4];//throws random garbage value if an array's element is not initialised with any value 
+     return 0;
+    }*/
+//--------------------------strings------------------------
+/*int main(){
+     string s = "Hariish";
+     int len = s.size();
+     s[len-1] = 't';
+     cout<<s;//every character in string is stored in form of array which means it can also perform basic array manipulations and each element is character which is stored using single inverted commas '' 
+     return 0;
+    }*/
+   int main(){
+     int arr[0][1];
+     arr[0][0] = 23;
+     cout<<arr[0][0];// prints the assigned value 
+     cout<<arr[0][1];//throws random garbage value if an array's element is not initialised with any value 
+     return 0;
+    }
+
+    
+
