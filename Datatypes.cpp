64d76e615081a98e3b01f3e3f7e8be9f710f0f71
Monika <3891monikasingh@gmail.com>
2025-10-20 18:54:25 +0530

    Add files via upload

---
+++ Datatypes.cpp
@@ -0,0 +1,63 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+/*int main(){
+int a;
+cin>>a;
+cout<<a+5;
+return 0;
+}*/
+//int,long,long long,
+/*for decimal - 
+float - 4 byte 
+double - 8 byte 
+long double - 10 byte */
+//----------------//
+/*int main(){
+    float a;
+    float b;
+    cin>>a>>b;
+    cout<<a<<" "<<b;
+    return 0;
+}
+    */
+   //-----------//
+/*int main(){
+    float a;
+    float b;
+    cin>>a>>b;
+    cout<<a+b;
+    return 0;
+}*/
+//-------------string datatype----------------
+// Two available functions - string and getline 
+/*int main(){
+    string s;
+    cin>>s; //intput -- hey striver 
+    cout<<s;// output -- hey - breaks after the first space introduced in the cin command,can input only single continous string of input without 
+    // space or line breakage  
+    return 0;
+}*/
+//-----------------------------------------
+/*int main(){
+    string s1,s2;
+    cin>>s1; //intput -- hey 
+    cin>>s2; //ip - striver 
+    cout<<s1+" "+s2;//output - hey striver- strings can be printed using + operator as well as stadarrd 
+    return 0;
+}*/
+//--------------- getline()------------------ 
+
+int main(){
+    string s;
+    getline(cin,s); //ip -- hii my name is hariish  
+    cout<<s;// op -- hii my name is hariish - accepts multiple words but does not accept multiple line of inputs 
+    return 0;
+} 
+
+
+
+
+
+
+  
