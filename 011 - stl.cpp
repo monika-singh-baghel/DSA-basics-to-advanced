168b73ce1487e7f701974c00ea386459d7acabff
Monika <3891monikasingh@gmail.com>
2025-10-23 18:10:19 +0530

    Update 011 - stl.cpp

--- 011 - stl.cpp
+++ 011 - stl.cpp
@@ -1,2 +1,5 @@
 /*STL stands for standard template library which contains a lot of pre-defined templates in terms of containers and classes which makes it very
 easy for programmers to implement different data structures easily without having to write complete code and worry about space-time complexities.*/
+
+/*1. Unordered set:- a container that stores unique elements in no particular order. Every operation on an unordered set takes O(1) complexity in the 
+average case and takes O(n) in the worst case.*/
