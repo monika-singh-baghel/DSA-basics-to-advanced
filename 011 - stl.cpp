57383347eaec928476014f13bbf2790bfb52f278
Monika <3891monikasingh@gmail.com>
2025-10-23 18:05:23 +0530

    Update 011 - stl.cpp

--- 011 - stl.cpp
+++ 011 - stl.cpp
@@ -1,1 +1,2 @@
-
+/*STL stands for standard template library which contains a lot of pre-defined templates in terms of containers and classes which makes it very
+easy for programmers to implement different data structures easily without having to write complete code and worry about space-time complexities.*/
