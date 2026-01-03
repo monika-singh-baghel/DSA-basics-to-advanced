da13642582a398c876c0a2edda59f26ccd809b1b
Monika <3891monikasingh@gmail.com>
2026-01-03 15:14:19 +0530

    Update 013 - basic_recursion.cpp

--- 013 - basic_recursion.cpp
+++ 013 - basic_recursion.cpp
@@ -1,1 +1,19 @@
+//-------------Print Name N times using Recursion------------
+#include <iostream>
+using namespace std;
+void printName(int i, int n)
+{  if (i > n)  // Base condition
+        return;
+    cout << "Ravi" << endl;
+ printName(i + 1, n); // Recursive call
+}
+int main()
+{
+    int n;
+    cout << "Enter number of times: ";
+    cin >> n;
+    printName(1, n);
+    return 0;
+}
+
 
