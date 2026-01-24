df9482c48cfa5d24214e913a254252bbd39e1664
Monika <3891monikasingh@gmail.com>
2026-01-24 10:28:57 +0530

    Update 014 - hashing.cpp

--- 014 - hashing.cpp
+++ 014 - hashing.cpp
@@ -1,3 +1,20 @@
+------------------//Frequency of elements using basic logic----------
+ #include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int number;
+    cin >> number;
+    int arr[5] = {1, 2, 1, 3, 2};
+    int cnt = 0;
+    for (int i = 0; i < 5; i++) {
+        if (arr[i] == number) {
+            cnt++;
+        }
+    }
+    cout << number << " -> " << cnt << " times";
+    return 0;
+}
+
 ------------------//Frequency of elements using Hash Array-----------------
  #include <bits/stdc++.h>
 using namespace std;
