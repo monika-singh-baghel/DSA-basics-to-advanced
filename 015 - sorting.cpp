f4bec36e40e27c5ce7b0e74960408b97ffe25e94
Monika <3891monikasingh@gmail.com>
2026-01-25 22:19:06 +0530

    Add selection sort implementation in sorting.cpp
    Implemented selection sort algorithm and added output display.

--- 015 - sorting.cpp
+++ 015 - sorting.cpp
@@ -1,1 +1,33 @@
+-----------------------//Selection Sort---------------------
+#include <bits/stdc++.h>
+using namespace std;
+void selection_sort(int arr[], int n) {
+  for (int i = 0; i < n - 1; i++) {
+        int mini = i; // Assume current index holds the minimum value
+        // Find the index of the smallest element in the remaining array
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[mini]) {
+                mini = j; // Update mini if smaller element is found
+            }
+        }
+        // Swap the found minimum element with the first element of unsorted part
+        int temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i] = temp;
+    }
+    cout << "After selection sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+int main() {
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    // Call selection sort
+    selection_sort(arr, n);
+    return 0;}
 
+----------------------//Bubble sort-----------------------
+
+  
